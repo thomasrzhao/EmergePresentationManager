930910d2314bc2a0717a7b05c7253658b8541e68
Thomas Zhao <trzhao@stanford.edu>
2015-08-13 16:51:11 +0900

    Initial Commit

---
+++ EmergePresentationManager/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EmergePresentationManager
+//
+//  Created by Thomas Zhao on 8/13/15.
+//  Copyright (c) 2015 Thomas Zhao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
